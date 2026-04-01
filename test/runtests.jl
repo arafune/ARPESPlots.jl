3ddb8c30638a4bdee46764fbe0e94ce53405ad43
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2026-04-01 10:44:47 +0900

    :white_check_mark:

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,8 @@ using ARPESPlots
 using Test
 
 @testset "ARPESPlots.jl" begin
-    # Write your tests here.
+    @testset "test for crosshair_heatmap" begin    # Write your tests here.
+        include("./crosshair_heatmap.jl")
+        include("./waterfall.jl")
+    end
 end
