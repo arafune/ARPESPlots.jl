dd6b9f5b3462ed571053500a2ce2b5bd94ea3c82
Ryuichi Arafune <ryuichi.arafune@gmail.com>
2026-04-09 22:26:19 +0900

    :art:

--- src/stitch.jl
+++ src/stitch.jl
@@ -17,23 +17,27 @@ function stitch_ui(
     heatmap_setting = merge(default_heatmap_setting, NamedTuple(heatmap_kwargs))
 
     fig = Figure(; fig_kwargs...)
-    ax = Axis(fig[1, 1])
+    ax = Axis(
+        fig[1, 1],
+        xlabel = DimensionalData.Dimensions.label(dims(A, 1)),
+        ylabel = DimensionalData.Dimensions.label(dims(A, 2)),
+    )
     slider_layout = GridLayout(fig[2, 1], tellwidth = false)
     sg = SliderGrid(
         slider_layout[1, 1],
         (label = "Seam Ratio", range = 0:0.001:1, startvalue = 0.5, width = 200),
-        (label = "Gain A(1st Arg.)", range = 0:0.001:1, startvalue = 1, width = 200),
+        (label = "Gain A(1st Arg.)", range = 0:0.002:3, startvalue = 1, width = 200),
         width = 500,
         tellheight = true,
     )
     seam_ratio_obs = sg.sliders[1].value
     gain_a_obs = sg.sliders[2].value
     C = lift(seam_ratio_obs, gain_a_obs) do r, g
-        stitch_along(A, B, dim; seam_ratio = r, gain = g)
+        stitch_along(A, B, dim; seam_ratio = r, gain_a = g)
     end
     x_obs = lift(s -> lookup(s, 1), C)
     y_obs = lift(s -> lookup(s, 2), C)
     updated_C = lift(parent, C)
-    heatmap!(ax, x_obs, y_obs, updated_C, heatmap_setting...)
+    heatmap!(ax, x_obs, y_obs, updated_C; heatmap_setting...)
     fig
 end
